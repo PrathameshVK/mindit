e34cc6c2d59245df129f2bf2646e750f0c6284d5
Prathamesh Kulkarni <prathameshvk50@gmail.com>
2022-02-26 23:08:23 +0530

    Added bcrypt for password string

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt=require('bcryptjs');
 const mongoose=require('mongoose');
 
 const Schema=mongoose.Schema;
@@ -7,5 +8,12 @@ const userSchema=new Schema({
     email: {type: String, required: true},
     password: {type: String, require: true}
 });
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+})
 
 module.exports=mongoose.model('user',userSchema,'users');
