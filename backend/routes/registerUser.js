714b4cc5f81c421a30a4e88adbe754e4a491c932
Prathamesh Kulkarni <prathameshvk50@gmail.com>
2022-02-27 14:05:33 +0530

    Added unique property to username and email in user registration.

--- backend/routes/registerUser.js
+++ backend/routes/registerUser.js
@@ -7,7 +7,11 @@ router.get('/',async function(req,res,next){
     const user=new User(userData);
     user.save((error,success)=>{
         if(error){
-            console.log(error);
+            if(error.keyValue && error.keyValue.email){
+                return res.status(422).send({ succes: false, message: 'Usern with that email already exists !' });
+            }else if(error.keyValue && error.keyValue.username){
+                return res.status(422).send({ succes: false, message: 'Usern with that username already exists !' });
+            }
         }else{
             res.status(200).send(success);
         }
